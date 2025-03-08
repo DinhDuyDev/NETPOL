2f21c63e16d9549980b456372f2f1e4396424720
Sandy <dinhduygiangvo@gmail.com>
2025-03-08 20:03:19 +0700

    Added a browser
    Added a browser

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-let 
+/**
+ * Defining a few constants
+ */
+
+//#region Search Boxes
+const webContent = document.getElementById("results-section");
+const searchBox = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+//#endregion
+
+//#region All Users
+const listOfElements = {
+    // website
+    "NETPOL": "netpol/netpol.html",
+    // users
+}
+
+/**
+ * Defining a few variables
+ */
+
+let searchRequest = "";
+
+/**
+ * Defining a few functions
+ */
+
+function getSearchInput() {
+    searchRequest = searchBox.value;
+    searchInput();
+}
+function searchInput() {
+    for (const [key, value] of Object.entries(listOfElements)) {
+        if (key == searchRequest) {
+            window.location.href = value;
+        }
+    }
+}
+/**
+ * Defining logic
+ */
+
+searchButton.onclick = getSearchInput;
